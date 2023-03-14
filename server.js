b0fb4ba3d123f913edb7cc8330dbf7e463705a6a
alans200 <asaen200@my.epcc.edu>
2023-03-14 13:29:28 -0600

    First layout of toolie web

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.use(express.static(__dirname + "/public"))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html')
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/contact.html')
+})
   
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
